8ed49471ce3213ac3676854d66e2b9023c5cd70e
schoeneberg <schoeneberg@github.com>
2024-02-28 17:53:10 +0100

    Restored a setup.py since on some systems pyproject.toml is apparently insufficient if the python version is very outdated

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name="lym1d",
+      description='Lyman-Alpha 1D power spectrum analysis',
+      author='Nils Schöneberg',
+      author_email='nils.science@gmail.com',
+      url="https://github.com/schoeneberg/lym1d",
+      install_requires=["numpy", "scipy", "george"],
+      packages=['lym1d'],
+      package_dir={'':'src'},
+      readme="README.md",
+      setuptools_git_versioning={'enabled':True},
+      setup_requires=["setuptools >= 61.0","setuptools-git-versioning"],
+      classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.8']
+      )
