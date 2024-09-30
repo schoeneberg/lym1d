4bf483ce38d3c14aa7545420309dced8aa9d251c
schoeneberg <schoeneberg@github.com>
2024-09-30 12:19:28 +0200

    Updated wrapper such that the old taylor results can be reproduced

--- src/lym1d_wrapper/cobaya_wrapper.py
+++ src/lym1d_wrapper/cobaya_wrapper.py
@@ -2,6 +2,7 @@ from cobaya.likelihood import Likelihood
 from .wrapper import lym1d_wrapper
 import os
 import numpy as np
+from scipy.interpolate import CubicSpline
 
 class cobaya_wrapper(Likelihood):
 
@@ -15,49 +16,24 @@ class cobaya_wrapper(Likelihood):
   #emupath ="nyx_files/lym1d_full_emulator_Oct2023_LP.npz"
   #data_path = "data_files/Chabanier19/"
   arguments = {'has_cor':None}
-
-  @classmethod
-  def getDefaultParameterBasis(cls):
-    pars = {}
-    N_redshift_DEFAULT = 13
-    for i in range(N_redshift_DEFAULT):
-      pars['normalization{}'.format(i+1)]=1
-    for i in range(N_redshift_DEFAULT):
-      pars['noise{}'.format(i+1)]={'prior':{'min':-1,'max':1},'ref':{'min':-1e-2,'max':1e-2}}
-
-    pars.update({
-         'A_lya':{'prior':{'min':7,'max':9},'ref':{'min':7.9,'max':8}},
-         'n_lya':{'prior':{'min':-2.47142933875679,'max':-2.2485707267925283},'ref':{'min':-2.4,'max':-2.3}},
-         'AmpTauEff':{'prior':{'min':0,'max':1.5},'ref':{'min':0.4,'max':0.44}},#0.42
-         'SlopeTauEffInf':{'prior':{'min':0,'max':7},'ref':{'min':3.5,'max':4.0}},#3.8
-         'SlopeTauEffBreak':0,
-         'T0':{'prior':{'min':0,'max':25000},'ref':{'min':6000,'max':10000}},#8000
-         'T0SlopeInf':{'prior':{'min':-5,'max':2},'ref':{'min':-2,'max':-1.5}},#8000
-         'T0SlopeBreak':{'prior':{'min':-10,'max':10},'ref':{'min':0,'max':0.1}},#8000
-         'gamma':{'prior':{'min':0.3,'max':2.0},'ref':{'min':1.1,'max':1.5}},#1.3
-         'gammaSlopeInf':{'prior':{'min':-5,'max':2},'ref':{'min':0,'max':0.1}},#0
-         'gammaSlopeBreak':0,
-         'A_UVB':{'prior':{'min':0,'max':2},'ref':{'min':0.9,'max':1.1}},
-         'Lya_DLA':{'prior':{'min':0,'max':1}},
-         'Lya_SN':{'prior':{'min':0,'max':1}},
-         'Lya_AGN':{'prior':{'min':0,'max':1}},
-         'Lya_UVFluct':0,
-         'fSiIII':{'prior':{'min':-1e-2,'max':1e-2}},
-         'fSiII':{'prior':{'min':-1e-2,'max':1e-2}},
-         'ResoAmpl':0,
-         'ResoSlope':0,
-         'SplicingCorr':0,
-         'SplicingOffset':0
-         })
-    return pars
-
+  # These are just DEFAULT option, feel free to overwrite with your own!!
+  params = {'T0':{'prior':{'min':0,'max':25000},'ref':{'min':6000,'max':10000}},#8000
+            'T0SlopeInf':{'prior':{'min':-5,'max':2},'ref':{'min':-2,'max':-1.5}},#8000
+            'T0SlopeBreak':{'prior':{'min':-10,'max':10},'ref':{'min':0,'max':0.1}},#8000
+            'gamma':{'prior':{'min':0.3,'max':2.0},'ref':{'min':1.1,'max':1.5}},#1.3
+            'gammaSlopeInf':{'prior':{'min':-5,'max':2},'ref':{'min':0,'max':0.1}},#0}
+            'gammaSlopeBreak':0,
+            'AmpTauEff':{'prior':{'min':0,'max':1.5},'ref':{'min':0.4,'max':0.44}},#0.42
+            'SlopeTauEffInf':{'prior':{'min':0,'max':7},'ref':{'min':3.5,'max':4.0}},#3.8
+            'SlopeTauEffBreak':0,
+            }
   def initialize(self):
 
     print("[lym1d_cobaya_wrapper] Beginning likelihood initialization")
     self.wrapper = lym1d_wrapper(
         runmode=self.runmode,
         base_directory = self.base_directory,
-        **self.arguments
+        **(self.arguments.copy())
         )
     nuisance = self.wrapper.nuisance_parameters
     print("[lym1d_cobaya_wrapper] nuisance parameters: ",nuisance)
@@ -71,26 +47,52 @@ class cobaya_wrapper(Likelihood):
     return self.sampled_params
 
   def get_requirements(self):
-    return {'Hubble':{'z':0},'Omega_m':None,'z_reio':None,'Pk_interpolator':{'z':[0,10],'k_max':10,'nonlinear':False}}
+    self.zs = np.linspace(0,10,num=100)
+    # Just some garbage to satisfy cobaya's policy of not computing anything unless specifically tasked to do so
+    return {'Hubble':{'z':self.zs},'Omega_m':None,'Omega_nu_massive':{'z':[0]},'sigma8_z':{'z':[0]},'z_reio':None,'Pk_interpolator':{'z':[0,10],'k_max':10,'nonlinear':False}}
 
   def logp(self, **params):
-    #print("params = ",params)
 
-    cosmo = self.provider.requirement_providers['Hubble'].classy
+    ##print("comso = ",cosmo.get_current_derived_parameters(['A_s'])['A_s'], cosmo.n_s(), cosmo.Omega_m(), cosmo.h(), cosmo.sigma8())
+
+    Omega_m = self.provider.get_param('Omega_m')
+    h = self.provider.get_param('h')
+    Omega_nu = self.provider.get_Omega_nu_massive(0)[0]
+    Hubble = CubicSpline(self.zs,self.provider.get_Hubble(self.zs))
+    sigma8 = self.provider.get_sigma8_z(0)[0]
+    n_s = self.provider.get_param('n_s')
+    pars = {'z_reio':self.provider.get_param('z_reio')}
+
+    class Container(object):
+      pass
+    FakeCosmo = Container()
+    FakeCosmo.Omega_m = lambda : Omega_m
+    FakeCosmo.h = lambda : h
+    FakeCosmo.Hubble = lambda : Hubble
+    FakeCosmo.Omega_nu = Omega_nu
+    FakeCosmo.sigma8 = lambda : sigma8
+    FakeCosmo.n_s = lambda : n_s
+    FakeCosmo.get_current_derived_parameters = lambda xs:{x:pars[x] for x in xs}
+
+    cosmo = FakeCosmo
+    #cosmo = self.provider.requirement_providers['Hubble'].classy
 
     try:
       chi2 = self.wrapper.chi2(cosmo,params)
     except ValueError as ve:
       print("[lym1d_cobaya_wrapper] : Encountered value error : ",ve)
+      import traceback
+      print(traceback.format_exc())
       raise #return -np.inf
     except KeyError as ke:
       print("[lym1d_cobaya_wrapper] : Encountered missing key : ",ke)
       raise
-    if chi2==None:
+
+    if chi2==None or np.isnan(chi2):
       return -np.inf
     else:
       return -0.5*chi2
 
 # VERY important line to correclty setup cobaya object (since cobaya inspects the CLASS, not the instance (!), therefore the class needs to be modified)
 # Also, since the code for setting up the default parameter basis is not 100% trivial, we need to do it in a function
-cobaya_wrapper.params = cobaya_wrapper.getDefaultParameterBasis()
+#cobaya_wrapper.params = cobaya_wrapper.getDefaultParameterBasis()
