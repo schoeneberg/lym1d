9828eaae473f89a3e4b1b21b2b6410495f0e9f65
schoeneberg <schoeneberg@github.com>
2024-11-25 19:44:03 +0100

    Just a slight update of the util to extinguish a bug where update accidentially added keys to the optiondict instead of giving error messages

--- src/lym1d/util.py
+++ src/lym1d/util.py
@@ -2,42 +2,51 @@
 Very simple utility file with various definitions of useful classes/functions
 (currently only OptionDict)
 """
-
 # Small utility class to make passing of arguments safer
 # You can only pass boolean values
 # You can only set specific options to true or false
 # You can iterate easily through only true or false options
 # You can add keys a posteriori
 class OptionDict(dict):
+
   # Initialize the OptionDict with dictionary default_values
   # The keys of the dictionary will be the allowed options
   # The values of the dictionary provide the default boolean option values
-  def __init__(self,default_values):
+  def __init__(self, default_values):
     self._keys = list(default_values.keys())
     for key in self._keys:
       self[key] = default_values[key]
+
   # Setting an item only works if the key is in the list of keys
   def __setitem__(self, key, val):
     if key not in self._keys:
-      raise KeyError
+      raise KeyError(key)
     if type(val)!=bool:
-      raise Exception("option can only be bool")
+      raise ValueError("Any option can only be bool! You tried to set '{}'='{}'".format(key,val))
     dict.__setitem__(self, key, val)
+
   # Iterate through all true options
   def iterate(self):
     for key in self._keys:
       if self[key]==True:
         yield key
     return
+
   # Iterate through all false options
   def inverse_iterate(self):
     for key in self._keys:
       if self[key]==False:
         yield key
     return
+
   # Add additional options (and give their correspoinding default values) with dictionary default_values
   def addkeys(self,default_values):
     self._keys += list(default_values.keys())
     for key in default_values.keys():
       self[key] = default_values[key]
+
+  # Make sure that the update routine behaves as expected, not (!) putting new options in!
+  def update(self, dic):
+    for k,v in dic.items():
+      self[k]=v
 
